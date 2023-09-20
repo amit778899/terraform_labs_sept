6a3fcb0f81b679a3d5240f2c86134e01ad851bb6
Amit Deshpande <amit.deshpande@sungardas.com>
2023-09-20 16:25:10 +0530

    hello

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "ami" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "instance_count" {
+  type = number
+}
+variable "region" {
+  type = string
+}
+variable "tags" {
+  type = string
+}
+variable "owner" {
+  type = string
+}
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
